d3217aa7676db3b120591ca087bce9b82c56c1bb
Angel Allembyjerath <159155426+YOSHAAAAAA@users.noreply.github.com>
2024-03-08 15:02:29 +0000

    EXPRESION

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -23,4 +23,17 @@ public class Principal {
         System.out.print("distancia = " +d+ "mts");
         teclado.close();
     }
+    public static void calcularExpresion(){
+        Scanner teclado = new Scanner(System.in);
+         float r;
+         int a,b;
+         System.out.println("proporciona valor de a:");
+         a = teclado.nextInt();
+         System.out.println("proporciona valor de b:");
+         b = teclado.nextInt();
+         r = (a+b)^2/(3*b);
+        // r = math.pow(a+b),2/(3*b);
+         System.out.println("resultado = "+r);
+         teclado.close();
+    }
 }
