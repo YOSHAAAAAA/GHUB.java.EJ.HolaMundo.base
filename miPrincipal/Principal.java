e5bfd1c8381133d5a0b3b7ba3f4fea2e520fddc8
Angel Allembyjerath <159155426+YOSHAAAAAA@users.noreply.github.com>
2024-03-08 14:39:32 +0000

    actualizacion.distancia

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,10 +1,26 @@
 package miPrincipal;
 
-
+import java.util.Scanner;
 
 public class Principal {
     
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        calcularDistancia();
+        
+    }
+    public static void calcularDistancia(){
+        Scanner teclado = new Scanner(System.in);
+        float d;
+        float v,t;
+        System.out.print("proporciona velocidad mts/seg:");
+        v = teclado.nextFloat();
+
+        System.out.print("proporciona el tiempo seg:");
+        t = teclado.nextFloat();
+
+        d = v*t;
+
+        System.out.print("distancia = " +d+ "mts");
+        teclado.close();
     }
 }
