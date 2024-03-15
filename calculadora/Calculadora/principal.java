3b29ca70d5cb0ef198c0cb3045a56d1825ad86c2
Angel Allembyjerath <159155426+YOSHAAAAAA@users.noreply.github.com>
2024-03-15 14:58:54 +0000

    Actualizacion

---
+++ calculadora/Calculadora/principal.java
@@ -0,0 +1,15 @@
+package Calculadora;
+
+public class principal {
+    public static void main(String[]args){
+        Calculadora objCalculadora = new Calculadora();
+        objCalculadora.Suma();
+        objCalculadora.resta();
+        objCalculadora.mult();
+        objCalculadora.div();
+        objCalculadora.res();
+          
+    }
+    
+    
+}
