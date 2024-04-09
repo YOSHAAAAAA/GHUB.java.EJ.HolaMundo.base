50731724866261c1ebd6673ba0c6b3049e882002
Angel Allembyjerath <159155426+YOSHAAAAAA@users.noreply.github.com>
2024-04-09 14:56:04 +0000

    actualizacion

---
+++ diferenciaEdades/principal.java
@@ -0,0 +1,10 @@
+package diferenciaEdades;
+
+
+public class principal {
+    public static void main(String[] args) {
+        DIFedad Dead = new DIFedad();
+        Dead.DIFedad();
+    }
+    
+}
