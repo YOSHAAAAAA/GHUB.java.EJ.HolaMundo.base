99d31cd8a2022a0e4d7cd5ae00c9c9b0000d3f81
Angel Allembyjerath <159155426+YOSHAAAAAA@users.noreply.github.com>
2024-04-24 14:58:43 +0000

    actualizacion

---
+++ demofor/DemoFor.java
@@ -0,0 +1,18 @@
+package demofor;
+
+public class  DemoFor{
+    public void demofor(){
+        int i;
+        System.out.println("ejemplos de ciclos ascedentes:");
+        for(i=1; i<=10;i++){
+            System.out.println("i="+i+"hola"+"\n");
+        }
+        System.out.println("ejemplos de ciclos ascedentes:");
+        for(i=1; i<=10;i+=2){
+            System.out.println("i="+i+"hola"+"\n");
+
+
+
+        }
+    }
+}
