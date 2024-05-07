72b3373a8ea3d6517d176082710b9274fb7af563
Angel Allembyjerath <159155426+YOSHAAAAAA@users.noreply.github.com>
2024-05-07 14:59:35 +0000

    actualizacion

---
+++ ecuacion/ecuacion.java
@@ -0,0 +1,15 @@
+package ecuacion;
+
+public class ecuacion {
+    public void ecuacion(){
+        int y;
+        int x=1;
+        while (x<=10){
+            y = x + 12;
+            System.out.println("x = "+x);
+            System.out.println("y= "+y);
+            x++;
+        }
+    }
+    
+}
